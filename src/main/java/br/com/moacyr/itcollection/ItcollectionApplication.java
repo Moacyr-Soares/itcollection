094cf93c37fc73b265efc0a056b910a5c7d4a5d8
moacyr <soares.moacyr.r@gmail>
2024-07-09 16:33:51 -0300

    Starting the project

---
+++ src/main/java/br/com/moacyr/itcollection/ItcollectionApplication.java
@@ -0,0 +1,13 @@
+package br.com.moacyr.itcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItcollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItcollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/moacyr/itcollection/ItcollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moacyr.itcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItcollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
